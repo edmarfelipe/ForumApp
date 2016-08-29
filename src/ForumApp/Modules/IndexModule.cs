4e57ea5edcfdef50bec9515023fe2d496d898c80
Edmar Felipe <edmarfelipe@hotmail.com>
2016-08-29 12:45:45 -0300

    initial commit

---
+++ src/ForumApp/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace ForumApp
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["Index"];
+            };
+        }
+    }
+}
---
+++ src/ForumApp/Modules/TopicModule.cs
@@ -0,0 +1,27 @@
+﻿using Nancy;
+
+namespace ForumApp.Modules
+{
+    public class TopicModule : NancyModule
+    {
+        public TopicModule()
+        {
+            Get["/Topics"] = parameters => 
+            {
+                return View["Topics"];
+            };
+
+            Get["/Topic/New"] = parameters =>
+            {
+                return View["NewTopic"];
+            };
+
+            Get["/Topic/{id:int}"] = parameters =>
+            {
+                var a = parameters.value;
+
+                return View["Topic"];
+            };
+        }
+    }
+}
